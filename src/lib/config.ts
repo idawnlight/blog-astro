363621d76e11b28395716122db90748533fcbd86
idawnlight <idawn@live.com>
2025-09-15 15:50:12 +0900

    feat(dark): disqusjs

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,9 +1,21 @@
+import type { DisqusJSConfig } from "disqusjs/react/es2022";
+
+type Without<T, K> = Pick<T, Exclude<keyof T, K>>;
+
 interface Config {
     name: string;
     description: string;
+    disqusJs?: Without<DisqusJSConfig, 'url'>;
 }
 
 export default {
     name: '余光的部落格',
     description: 'For the next infinity.',
+    disqusJs: {
+        shortname: 'dawnlight',
+        api: "https://idawnlight.com/api/",
+        apikey: "qMzZgFvrD6nma2S9G8xjOT7OmyBNOMsv4ZC9CetPmqbq94zn8W66Zpwg7PAuJbmt",
+        admin: '黎明余光',
+        adminLabel: 'Admin',
+    },
 } as Config;
